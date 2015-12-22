5cd3b83622ca8b4a6662bb2abea2d7a96c6d7bbb
Eamon O'Dea <odea35@gmail.com>
2015-12-22 11:27:02 -0500

    Put detrending and smoothing code into own functions
    This smoothing code can be reused to optionally smooth the estimated
    cross correlations. We have also changed the code style to satisfty
    the default guidelines of lintr.

--- R/analysis.R
+++ R/analysis.R
@@ -1,5 +1,5 @@
 
-#' Estimate the autocorrelation at a given lag over rolling windows
+#' Detrend ensemble time series
 #'
 #' @param x A univariate or multivariate numeric time series object or
 #'   a numeric vector or matrix.
@@ -11,55 +11,54 @@
 #'   '"covariance"'. Will be partially matched.
 #' @param na.action Function to be called to handle missing
 #'   values. 'na.pass' can be used.
-#' @param detrend Character string giving method of centering the
-#'   series before calculating the ACF. Allowed values are '"grand.mean"'
-#'   (the default), '"ensemble.means"', '"local.mean.gaussian.weights"',
-#'   '"local.linear.gaussian.weights"', '"none"'. Will be partially matched.
-#' @param bandwidth.detrend Bandwith for any kernal-based detrending
+#' @param type Character string giving method of calculating the trend
+#' to subtract. Allowed values are '"grand.mean"' (the default),
+#' '"ensemble.means"', '"local.mean.gaussian.weights"',
+#' '"local.linear.gaussian.weights"', '"none"'. Will be partially
+#' matched.
+#' @param bandwidth Bandwith for any kernel-based detrending
 #'   done. If not supplied, a bandwidth will be selected by
 #'   cross-validation of least squared error.
 #' @export
 #'
-#'
-GetRollingACF <- function(x, lag=1, type=c("correlation", "covariance"),
-                          na.action=na.fail, detrend=c("grand.mean",
-                                                 "ensemble.means",
-                                                 "local.constant.gaussian.weights",
-                                                 "local.linear.gaussian.weights",
-                                                 "none"),
-                          bandwidth.detrend=NULL){
-  ## First few lines from stats::acf() function
-  detrend <- match.arg(detrend)
+detrend <- function(x, na.action=na.fail,
+                    type=c("grand.mean", "ensemble.means",
+                        "local.constant.gaussian.weights",
+                        "local.linear.gaussian.weights"),
+                    bandwidth=NULL){
   type <- match.arg(type)
-  x <- na.action(as.ts(x))
-  x.freq <- frequency(x)
+  x <- na.action(x)
   x <- as.matrix(x)
   if (!is.numeric(x)) stop("'x' must be numeric")
-  sampleT <- as.integer(nrow(x))
-  nser <- as.integer(ncol(x))
+
   rmn <- rowMeans(x, na.rm=TRUE)
-  step <- seq(1, sampleT)
-  if (detrend == "grand.mean"){
+  if (type == "grand.mean"){
     x <- x - mean(rmn)
-  } else if (detrend == "ensemble.means"){
+  } else if (type == "ensemble.means"){
     x <- x - rmn
-  } else if (detrend == "local.constant.gaussian.weights"
-             || detrend == "local.linear.gaussian.weights"){
+  } else if (type == "local.constant.gaussian.weights"
+             || type == "local.linear.gaussian.weights"){
+    samplet <- as.integer(nrow(x))
+    step <- seq(1, samplet)
     data <- data.frame(step=step, rmn=rmn)
-    is.constant <- grepl('constant', detrend)
-    rt <- ifelse(is.constant, 'lc', 'll')
-    if (!is.null(bandwidth.detrend)){
-      bw <- np::npregbw(formula=rmn ~ step, bws=bandwidth.detrend,
-                        regtype=rt, ckertype="gaussian", ckerorder=2,
-                        bandwidth.compute=FALSE, data=data)
-    } else {
-      bw <- np::npregbw(formula=rmn ~ step, regtype=rt, ckertype="gaussian",
-                        bkerorder=2, bwmethod='cv.ls', bwtype='fixed',
-                        data=data)
-    }
-    mod <- np::npreg(bw)
-    srm <- fitted(mod)
+    srm <- smooth(data, type, bandwidth)
     x <- x - srm
   }
   x
+}
+
+smooth <- function(data, type, bandwidth){
+  is.constant <- grepl("constant", type)
+  rt <- ifelse(is.constant, "lc", "ll")
+  if (!is.null(bandwidth)){
+    bw <- np::npregbw(formula=rmn ~ step, bws=bandwidth,
+                      regtype=rt, ckertype="gaussian", ckerorder=2,
+                      bandwidth.compute=FALSE, data=data)
+  } else {
+    bw <- np::npregbw(formula=rmn ~ step, regtype=rt, ckertype="gaussian",
+                      bkerorder=2, bwmethod="cv.ls", bwtype="fixed",
+                      data=data)
+  }
+  mod <- np::npreg(bw)
+  fitted(mod)
 }
