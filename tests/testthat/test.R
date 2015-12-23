2c962bd08e6e12121fa644e5d2b34ba02ccc478e
Eamon O'Dea <odea35@gmail.com>
2015-12-22 19:17:36 -0500

    Add test of local linear smoother

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -21,21 +21,41 @@ test_that("Mean-based detrending works", {
 context("smoothing")
 
 test_that("Smoothing function works as expected", {
-  data <- data.frame(step=1:10, rmn=1:10)
-  simple_smooth <- function(data, bandwidth){
+  simple_smooth <- function(data, type, bandwidth){
     kern <- function(ind, bw=bandwidth){
       dist <- abs(data$step - ind) / bw
       w <- dnorm(dist)
       w / sum(w)
     }
     w <- sapply(data$step, kern)
-    colSums(w * data$rm)
+    if (type == "local.constant"){
+      colSums(w * data$rm)
+    } else {
+      wlm <- function(x){
+        m <- lm(rmn~step, weights=w[, x], data=data)
+        fitted(m)[x]
+      }
+      sapply(seq_along(data$step), wlm)
+    }
   }
+  data <- data.frame(step=1:10, rmn=1:10)
   expect_equal(smooth(data, type="local.constant", bandwidth=2),
-               simple_smooth(data, bandwidth=2))
+               simple_smooth(data, type="local.constant",  bandwidth=2))
+  expect_equal(smooth(data, type="local.linear", bandwidth=2),
+               simple_smooth(data, type="local.linear",  bandwidth=2),
+               check.names=FALSE)
+
   expect_equal(smooth(data, type="local.constant", bandwidth=3),
-               simple_smooth(data, bandwidth=3))
+               simple_smooth(data, type="local.constant", bandwidth=3))
+  expect_equal(smooth(data, type="local.linear", bandwidth=3),
+               simple_smooth(data, type="local.linear", bandwidth=3),
+               check.names=FALSE)
+
   data2 <- data.frame(step=20:100, rmn=rnorm(81))
   expect_equal(smooth(data2, type="local.constant", bandwidth=5),
-               simple_smooth(data2, bandwidth=5))
+               simple_smooth(data2, type="local.constant", bandwidth=5))
+  expect_equal(smooth(data2, type="local.linear", bandwidth=5),
+               simple_smooth(data2, type="local.linear", bandwidth=5),
+               check.names=FALSE)
+
 })
