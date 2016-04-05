917e51ea6c10cfcc54cffe0678f4b11f3e52f4ce
Eamon O'Dea <odea35@gmail.com>
2016-04-05 19:54:04 -0400

    Add test of simulator based on LNA

--- tests/testthat/test-simulator.R
+++ tests/testthat/test-simulator.R
@@ -16,8 +16,6 @@ test_that("Argument checking works", {
 
 context("Gillespie direct method simulator")
 
-
-
 test_that(paste("Mean and stddev of stationary model over time",
                 "consistent with ensemble mean and stdev of dizzy",
                 "progam's implementation"), {
@@ -36,7 +34,6 @@ test_that(paste("Mean and stddev of stationary model over time",
   expect_lt(abs(sd(so[, "I"]) - 0.5130), 0.25)
   expect_lt(abs(sd(so[, "S"]) - 9.9963), 0.1)
 })
-
 
 test_that(paste("Means and final stddev of time-dependent model",
                 "consistent with ensemble mean and stdev of dizzy",
@@ -96,4 +93,33 @@ test_that(paste("Means and final stddev of time-dependent model",
                check.attributes=FALSE, tol=0.25)
   expect_equal(sd(ens_susceptible[100, ]), tfsds["S"],
                check.attributes=FALSE, tol=0.25)
+})
+
+test_that(paste("Fluctuations for large system sizes approximate AR process",
+                "given by linear noise approximation"), {
+
+  params <- c(gamma=16.59091, mu=0.02, d=0.02, eta=2e-4, beta=100e-6,
+              rho=0.9, S_0=0.165779, I_0=0.001004, R_0=0.833216, N_0=1e6)
+  times <- seq(0, 1000, len=1000)
+  sim <- create_simulator(params=params, times=times, process_model="SIR")
+  so <- pomp::simulate(sim, as.data.frame=TRUE, seed=202)
+  sts <- so[, c("I", "S")]
+  sim_sigma <- cov(sts)
+  expect_equal(sim_sigma["I", "I"], 0.1095306 * params["N_0"],
+               check.attributes=FALSE, tolerance=0.2)
+  expect_equal(sim_sigma["S", "S"], 18.0094688 * params["N_0"],
+               check.attributes=FALSE, tolerance=0.2)
+  expect_equal(sim_sigma["I", "S"], -0.1298541 * params["N_0"],
+               check.attributes=FALSE, tolerance=0.2)
+
+  sim_ac1_II <- cov(sts[-1, "I"], sts[-nrow(sts), "I"]) / sim_sigma["I", "I"]
+  sim_ac1_IS <- (cov(sts[-1, "I"], sts[-nrow(sts), "S"])
+                   / prod(sqrt(diag(sim_sigma))))
+  sim_ac1_SI <- (cov(sts[-1, "S"], sts[-nrow(sts), "I"])
+                   / prod(sqrt(diag(sim_sigma))))
+  sim_ac1_SS <- cov(sts[-1, "S"], sts[-nrow(sts), "S"]) / sim_sigma["S", "S"]
+  expect_equal(sim_ac1_II, 0.2037399, tol=0.2)
+  expect_equal(sim_ac1_IS, 0.8501284, tol=0.2)
+  expect_equal(sim_ac1_SI,-0.9121943, tol=0.2)
+  expect_equal(sim_ac1_SS, 0.3079941, tol=0.2)
 })
