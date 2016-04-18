70c336dcb7da0765b64705a473f87c3a0d423f48
Eamon O'Dea <odea35@gmail.com>
2016-04-17 23:02:26 -0400

    Add test of calculation of cases

--- tests/testthat/test-simulator.R
+++ tests/testthat/test-simulator.R
@@ -17,6 +17,21 @@ test_that("Argument checking works", {
 
 context("Gillespie direct method simulator")
 
+test_that("Calculation of cases consistent with number of recovered", {
+  skip_if_not_installed("pomp")
+
+  params <- c(gamma=24, mu=0.014, d=0.0, eta=1e-4, beta=24e-2,
+              rho=0.9, S_0=1, I_0=0, R_0=0, N_0=1e2)
+  covar <- data.frame(gamma_t=c(0, 0), mu_t=c(0, 0), d_t=c(0, 0),
+                      eta_t=c(0, 0), beta_t=c(0, 0), time=c(0, 1e6))
+  times <- seq(0, 1000, by=1)
+
+  sim <- create_simulator(params=params, times=times, covar=covar,
+                          process_model="SIR")
+  so <- pomp::simulate(sim, as.data.frame=TRUE, seed=200)
+  expect_equal(diff(so$R), so$cases[-1])
+})
+
 test_that(paste("Mean and stddev of stationary model over time",
                 "consistent with ensemble mean and stdev of dizzy",
                 "progam's implementation"), {
